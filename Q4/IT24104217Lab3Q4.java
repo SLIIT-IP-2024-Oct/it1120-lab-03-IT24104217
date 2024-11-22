d59eda0ac2757c5def0a69027307af97fddd7a8b
IT24104217 <it24104217@my.sliit.lk>
2024-11-22 13:35:25 +0530

    Update IT24104217Lab3Q4.java

--- Q4/IT24104217Lab3Q4.java
+++ Q4/IT24104217Lab3Q4.java
@@ -2,28 +2,28 @@ import java.util.Scanner;
 
 public class IT24104217Lab3Q4 {
     public static void main(String[] args) {
-        // Create a Scanner object for user input
+        
         Scanner scanner = new Scanner(System.in);
 
-        // Prompt the user to enter a five-digit number
+        
         System.out.print("Enter a five-digit number: ");
-        int number = scanner.nextInt(); // Read the number
+        int number = scanner.nextInt(); 
 
-        // Check if the number is a five-digit number
+        
         if (number < 10000 || number > 99999) {
             System.out.println("Please enter a valid five-digit number.");
         } else {
-            // Convert the number to a string to separate digits
+            
             String numberStr = Integer.toString(number);
             
-            // Print each digit separated by a space
+            
             for (int i = 0; i < numberStr.length(); i++) {
                 System.out.print(numberStr.charAt(i) + " ");
             }
-            System.out.println(); // Print a new line at the end
+            System.out.println(); 
         }
 
-        // Close the scanner
+        
         scanner.close();
     }
 }
